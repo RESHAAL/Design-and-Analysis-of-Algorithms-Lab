e3995cdf7427294e9892b780322544c3488cc34b
RESHAAL <155475899+RESHAAL@users.noreply.github.com>
2025-07-18 13:20:10 +0530

    Add files via upload

---
+++ binarySearchUsingRecursion.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void bubbleSort(int arr[], int n)
+{
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int binarySearch(int arr[], int low, int high, int target)
+{
+    if(low>high)
+        return -1;
+
+    int mid;
+    mid=low+ (high-low)/2;
+
+    if(arr[mid] == target)
+        return mid;
+    else if(arr[mid]>target)
+        return binarySearch(arr, low, mid-1, target);
+    else
+        return binarySearch(arr, mid+1, high, target);
+}
+
+int main()
+{
+    int arr[] = {4,2,10,6,12,4,8};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    int target= 10;
+    int result;
+
+    bubbleSort(arr, n);
+     printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    result = binarySearch(arr, 0, n-1, target);
+    if(result!= -1)
+        printf("Element found at index %d", result);
+    else
+        printf("Element not found");
+}
